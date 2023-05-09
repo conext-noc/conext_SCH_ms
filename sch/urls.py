4350a0dcb5430856a6916e71dcb48a2a32eb5e47
Ricardo Vera <ricardovera71@gmail.com>
2023-05-08 23:02:41 -0500

    fix(url): added get status check @ root and moved main to client/

--- sch/urls.py
+++ sch/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SCH,CHECK,PWR
 
 urlpatterns = [
-    path("", SCH.as_view()),
-    path("check/", CHECK.as_view()),
+    path("client/", SCH.as_view()),
+    path("", CHECK.as_view()),
     path("power/", PWR.as_view())
 ]
