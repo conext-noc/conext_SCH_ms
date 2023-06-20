bf35c62c8f83d1dbea71aec3f2f60e9249073f89
Ricardo Vera <ricardovera71@gmail.com>
2023-06-20 13:17:45 -0500

    fix(pwr)

--- sch/urls.py
+++ sch/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-# from .views import SCH,CHECK,PWR
-from .views import SCH, CHECK
+from .views import SCH, CHECK, PWR
+
+# from .views import SCH, CHECK
 
 urlpatterns = [
     path("client/", SCH.as_view()),
     path("", CHECK.as_view()),
-    # path("power/", PWR.as_view())
+    path("power/", PWR.as_view()),
 ]
